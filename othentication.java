974ec27facf074baeaff1aee46d611f18fbf5f1b
Ritom kumar Hajra <ritomk61@gmail.com>
2026-08-09 23:52:34 +0530

    up

---
+++ othentication.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+/**
+ * othentication
+ */
+public class othentication {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+         System.out.println("enter your username:");
+        String username = sc.nextLine();
+
+        System.out.println("enter your pass:");
+        String pass = sc.nextLine();
+
+        if( username.equals("ritom") && pass.equals("123")){
+              System.out.println("login successfull");
+        }
+        else{
+            System.out.println("invalid username and password.try again");
+        }
+        sc.close();
+
+
+    }
+}
