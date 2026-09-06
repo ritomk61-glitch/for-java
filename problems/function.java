1cc6603b2ff6953068e43699ee3502ea2b4bf58d
Ritom kumar Hajra <ritomk61@gmail.com>
2026-09-06 22:22:13 +0530

    up

---
+++ problems/function.java
@@ -0,0 +1,6 @@
+/**
+ * function
+ */
+public class function {
+
+}
--- problems/p.java
+++ problems/p.java
@@ -1,5 +1,17 @@
-class p {
+class P {
+
+    static int sum(int n) {
+        if (n == 1 || n == 0) {
+            return 1;
+        }
+
+        return n + sum(n - 1);
+    }
+
     public static void main(String[] args) {
-        System.out.println("hello this is my first time java code ");
+
+        System.out.println("Hello, this is my first Java code");
+
+        System.out.println(sum(5));
     }
 }
