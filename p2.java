910756d89a16b8ee98e1721971681ea98e6fa701
Ritom kumar Hajra <ritomk61@gmail.com>
2026-09-17 11:46:08 +0530

    up

--- p2.java
+++ p2.java
@@ -1,3 +1,18 @@
 public class p2 {
-    
+    public static void main(String[] args) {
+     String name = "Ritom";
+     String name1 = "Ritom";
+
+     String name2 ="prianka";
+
+     System.out.println(name == name1 );
+     System.out.println(name1 == name2);
+     System.out.println(name1.hashCode());
+     System.out.println(name1.hashCode());
+     System.out.println(name2.hashCode());
+     System.out.println(name.hashCode() == name1.hashCode());
+     System.out.println(name.hashCode() == name2.hashCode());
+     System.out.println(name.equals(name1));
+     System.out.println(name1.equals(name2));
+    }
 }
